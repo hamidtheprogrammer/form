021678897c6501fcb701a307374e11ea2408e3a8
HAMID <owoyemiameed@gmaiil.com>
2024-05-21 19:26:24 +0100

    video section done

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const Video = () => {
+  useEffect(() => {
+    const video = document.querySelector(`video`);
+    const videoSection = document.querySelector(`#video`);
+    window.addEventListener("scroll", () => {
+      animateVideo();
+    });
+
+    function animateVideo() {
+      const { bottom } = video.getBoundingClientRect();
+      let scale = 1 - (bottom - window.innerHeight) * 0.0005;
+      scale = scale < 0.5 ? 0.5 : scale > 1 ? 1 : scale;
+
+      video.style.transform = `scale(${scale})`;
+    }
+  }, []);
   return (
     <section id="video">
       <div className="sticky">
-        <video src="https://framerusercontent.com/assets/BcIElVBzSD9P1ht5PhehnVyzTA.mp4"></video>
+        <video
+          autoPlay
+          muted
+          loop
+          playsInline
+          src="https://framerusercontent.com/assets/BcIElVBzSD9P1ht5PhehnVyzTA.mp4"
+        ></video>
       </div>
     </section>
   );
