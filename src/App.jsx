2d99967946bebabe6a678cef62568ae2b38d4710
HAMID <owoyemiameed@gmaiil.com>
2024-05-21 18:07:32 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Hero from "./components/Hero";
+import About from "./components/About";
+
+const App = () => {
+  return (
+    <div className="relative app w-[100%] h-[100%] ">
+      <div className="line_container">
+        <div className="separator"></div>
+        <div className="separator"></div>
+        <div className="separator"></div>
+      </div>
+      <main className="relative">
+        <Hero />
+        <About />
+      </main>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <section id="hero">
+      <div className="hero_container">
+        <div className="hero_title">
+          <h1 className="hero_title_header">FORM</h1>
+        </div>
+        <div className="hero_cta">
+          <h4>STUDIO</h4>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
