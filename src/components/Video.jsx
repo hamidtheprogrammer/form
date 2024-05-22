d7b1c5b01db0afbabb71c3d0f6e949532424d827
HAMID <owoyemiameed@gmaiil.com>
2024-05-22 19:24:46 +0100

    text added

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -12,13 +12,21 @@ const Video = () => {
       const { top, bottom } = videoSection.getBoundingClientRect();
       let scale = 1 - (bottom - window.innerHeight) * 0.0004;
       //   console.log(bottom);
-      scale = scale < 0.2 ? 0.2 : scale > 1 ? 1 : scale;
+      scale = scale < 0.5 ? 0.5 : scale > 1 ? 1 : scale;
 
       video.style.transform = `scale(${scale})`;
+
+      let textTrans = bottom - innerHeight;
+      const textHeaderleft = document.querySelector(`.text_header_left`);
+      const textHeaderright = document.querySelector(`.text_header_right`);
+
+      textHeaderleft.style.transform = `translateX(${-textTrans}px)`;
+      textHeaderright.style.transform = `translateX(${textTrans}px)`;
     }
   }, []);
   return (
     <section id="video">
+      <div className="shim"></div>
       <div className="sticky">
         <video
           autoPlay
@@ -27,6 +35,10 @@ const Video = () => {
           playsInline
           src="https://framerusercontent.com/assets/BcIElVBzSD9P1ht5PhehnVyzTA.mp4"
         ></video>
+        <div className="video_text_overlay">
+          <h2 className="text_header_left">SHOW</h2>
+          <h2 className="text_header_right">CASE</h2>
+        </div>
       </div>
     </section>
   );
